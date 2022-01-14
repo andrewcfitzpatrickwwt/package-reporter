de62c63f4e4841f27d2f9c336f88b007598d4e1f
Andrew Fitzpatrick <andrew.fitzpatrick@wwt.com>
2022-01-14 15:12:30 -0600

    stubbed out basic program

---
+++ src/WWT.PackageReporter/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Build.Construction;
+
+namespace WWT.PackageReporter;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var path = ParseArguments(args);
+        
+        var sln = SolutionFile.Parse("");
+    }
+    
+    private static string ParseArguments(string[] args)
+    {
+        if (!args.Any() || args[0] == "-h" || args[0] == "--help")
+        {
+            DisplayHelp();
+            Environment.Exit(0);
+        }
+
+        return args[0];
+    }
+
+    private static void DisplayHelp()
+    {
+        Console.WriteLine("Usage: dotnet run -- <solution path>");
+        Console.WriteLine("");
+        Console.WriteLine("Parse a solution's nuget package references and report on the versions.");
+        Console.WriteLine("");
+        Console.WriteLine("Options:");
+        Console.WriteLine("   -h, --help - Show help for this program");
+    }
+}
